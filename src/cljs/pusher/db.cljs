3f8faabe7a83578fa2b3c4b6b871f02e90566698
Stephen Wood <stephen@photovisionprints.com>
2017-03-03 17:21:47 -0800

    Add speeds to db

--- src/cljs/pusher/db.cljs
+++ src/cljs/pusher/db.cljs
@@ -6,6 +6,10 @@
            {:name "Kodak Portra 160" :speed 160 :optimal-speed 160}
            {:name "Kodak Portra 400" :speed 400 :optimal-speed 320}
            {:name "Kodak Portra 800" :speed 800 :optimal-speed 640}]
+   :speeds [1 1.2 1.6 2 2.5 3 4 5 6 8 10 12 16 20 25 32
+            40 50 64 80 100 125 160 200 250 320 400 500
+            640 800 1000 1250 1600 2000 2500 3200 4000
+            5000 6400 8000 10000 12500 16000 20000 25000]
    :selected-film "Fuji 400H"  
    :metered-speed 400
    :bulb-in false
